6e51bad259363a55d07971650102b230c5ddf110
DoWh <dinkudom@gmail.com>
2023-04-03 18:04:09 +0300

    install react, electron

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+// const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+const isMac = process.platform === 'darwin';
+const isDev = process.argv[2] === 'dev'
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+const createMainWindow = () => {
+    const win = new BrowserWindow({
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'preload.js')
+        // },
+        width: (isDev) ? 1000 : 400,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL('http://localhost:3000');
+
+    if (isDev) win.webContents.openDevTools()
+    
+} 
+
+app.whenReady().then(createMainWindow)
+
+app.on('window-all-closed', () => {
+    if (!isMac) app.quit()
+})
