6a84d04b1ea76ea6cbcd5dba292f41e3e3d298f1
DoWh <dinkudom@gmail.com>
2023-04-04 13:41:42 +0300

    add style to ServerHeader(component)

---
+++ src/components/BtnAddPortUnit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BtnAddPortUnit(){
+    return (
+        <>
+            BtnAddPortUnit
+        </>
+    )
+}
+
+export default BtnAddPortUnit;
--- src/components/ServerHeader.js
+++ src/components/ServerHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import BtnAddPortUnit from "./BtnAddPortUnit"
 
 function ServerHeader(){
     const [myIp, setMyIp] = useState('load ip...');
@@ -10,9 +11,11 @@ function ServerHeader(){
             setMyIp(await res.text())
         }
     });
-    return (
-        <header>
-            my ip: {myIp}, btnAddPortUnit
+    return (    
+        <header className="flex justify-between p-4 border-b-2">
+            <div className="basis-1/4 text-right p-1 opacity-25 select-none">My ip:</div>
+            <div className="basis-1/4 p-1 select-all">{myIp}</div>
+            <div className="basis-1/2 justify-self-end p-1"><BtnAddPortUnit/></div>
         </header>
     )
 }
